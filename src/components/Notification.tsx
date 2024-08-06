928bbc291b8eb7ba59f0040428f5dd55b2124eaf
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-08-07 00:17:41 +0530

    creating layout and nnotifiaction section styles

---
+++ src/components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-12 text-white bg-orange-500 px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Free delivery over $25 and lot of discounts.Order Now!
+    </div>
+  );
+};
+
+export default Notification;
