51596ce6ed259fb43593761aa22bdc7b6053926e
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-08-28 00:03:55 +0530

    home screen slider done.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import Featured from "@/components/Featured";
+import Slider from "@/components/Slider";
+import Offer from "@/components/Offer";
 
 export default function Home() {
-  return <main>hello World</main>;
+  return (
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
+  );
 }
